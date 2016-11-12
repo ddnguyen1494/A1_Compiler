213ff7e64622a58f374d42a4eaabb6cfc770b91a
Daniel Nguyen <duynguyen1494@icloud.com>
2016-11-12 09:26:03 -0800

    Add comments

--- A1_Compiler/pst.h
+++ A1_Compiler/pst.h
@@ -1,5 +1,9 @@
-/**
+/*
 * Created by Daniel Nguyen
+* Email: duynguyen1494@fullerton.edu
+* Created date: 09/02/16
+* Modified by Sae Hun Kim, Anthony Nguyen
+* Modified date: 09/11/16
 */
 
 #ifndef PST_H
@@ -10,27 +14,35 @@
 
 class Pst {
 private:
-	Node * _root;
-	Node * top_stack;
+	Node * _root;				//root of the tree
 
-	void clear(Node*);
+	Node * top_stack;			//top of the stack; primarily used to hook up kids' nodes to the mom
 
-	void print(Node *);
+	void clear(Node*);			//Clear function to deallocate every node of the tree
 
-	void p2acvt(Node *);
+	void print(Node *);			//print out node's data
+	
+	void p2acvt(Node *);			//Modify each node to make it ast's node
 public:
-	Pst();
+	//Constructor
+	Pst();					
+	
+	//Destructor
+	~Pst();					
+	
+	//Check if the tree is empty( has no node)
+	bool is_empty();			
 
-	~Pst();
-
-	bool is_empty();
-
-	Node * get_root();
-
+	//Getter function to retrieve the root of the tree
+	Node * get_root();			
+	
+	//Setter function for the root of the tree
 	void set_root(Node *);
-
+	
+	//A recursive function to traverse the tree preorder (used for printing out node's data)
 	void print_preorder(Node *);
 	
+	//A recursive function to traverse the tree preorder (used for converting pst to ast)
 	void p2ast(Node *);
 };
 #endif
